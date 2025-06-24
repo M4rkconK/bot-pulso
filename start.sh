72c45bf0edc3bd80c329ae00706e08e664b8244c
M4rkconK <dynoxmusicoficial@gmail.com>
2025-06-24 15:11:00 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,2 @@
-services:
-  - type: web
-    name: bot-pulso
-    env: python
-    buildCommand: ""
-    startCommand: bash start.sh
-    plan: free
-    envVars:
-      - key: DISCORD_TOKEN
-        value: TU_TOKEN_DISCORD
-      - key: PULSOID_TOKEN
-        value: TU_TOKEN_PULSOID
-      - key: CHANNEL_ID
-        value: TU_ID_DEL_CANAL
+#!/bin/bash
+python3 bot.py
